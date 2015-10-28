faba114229db547a7e16e694ed8c7e8fb09e8822
Luc <hardbarger@infoplanit.com>
2015-10-28 02:39:33 -0400

    Hook up api to remote source using ember data.

--- app/controllers/bands/band/songs.js
+++ app/controllers/bands/band/songs.js
@@ -12,7 +12,12 @@ export default Ember.Controller.extend({
     updateRating: function(params){
       var song = params.item,
         rating = params.rating;
+
+      if (song.get('rating') === rating){
+        rating = 0;
+      }
       song.set('rating', rating);
+      song.save();
     }
   }
 });
--- app/routes/bands/band/songs.js
+++ app/routes/bands/band/songs.js
@@ -1,20 +1,26 @@
 import Ember from 'ember';
-import Song from '../../../models/song';
 
 export default Ember.Route.extend({
+  model: function(){
+    return this.modelFor('bands.band');
+  },
   actions: {
     didTransition: function(){
       var band = this.modelFor('bands.band');
       document.title = `${band.get('name')} songs - Rock & Roll`;
     },
     createSong: function(){
-      var controller = this.get('controller');
-      var band = this.modelFor('bands.band');
-      var title = controller.get('title');
+      var controller = this.get('controller'),
+        band = this.modelFor('bands.band');
 
-      var song = Song.create({ title: title, band: band });
-      band.get('songs').pushObject(song);
-      controller.set('title', '');
+      var song = this.store.createRecord('song', {
+        title: controller.get('title'),
+        band: band
+      });
+
+      song.save().then(function(){
+        controller.set('title', '');
+      });
     }
   }
 });
